f30da3e1f2025dd7a913244cbf4acd21dde4ed8d
Filippo Cilia <filippocarlocilia@gmail.com>
2026-05-25 19:01:17 +0100

    base setup

---
+++ NineTilesPuzzle/Views/PuzzleView.swift
@@ -0,0 +1,18 @@
+//
+//  PuzzleView.swift
+//  NineTilesPuzzle
+//
+//  Created by Filippo Cilia on 5/25/26.
+//
+
+import SwiftUI
+
+struct PuzzleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PuzzleView()
+}
