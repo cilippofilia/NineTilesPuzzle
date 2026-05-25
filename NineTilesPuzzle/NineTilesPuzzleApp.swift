3da36e850897014112073f2a52687912f4cc51f5
Filippo Cilia <filippocarlocilia@gmail.com>
2026-05-25 18:50:31 +0100

    Initial Commit

---
+++ NineTilesPuzzle/NineTilesPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NineTilesPuzzleApp.swift
+//  NineTilesPuzzle
+//
+//  Created by Filippo Cilia on 5/25/26.
+//
+
+import SwiftUI
+
+@main
+struct NineTilesPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
